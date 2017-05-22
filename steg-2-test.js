476daa40ccd560e8b2868fd3ea1f3f8b443ade42
Dailyman <robin.lars.hakansson@gmail.com>
2017-05-22 14:56:18 +0200

    stuff

--- steg-2-test.js
+++ steg-2-test.js
@@ -2,34 +2,38 @@ $(document).ready(function() {
     $('select').material_select();
 
     $("#event-image-preview, #event-banner-preview, #track-maps-preview").hide();
+
+    $(window).scroll(function() {
+
+    });
 });
 
 function readURL(input, imageId) {
     $(imageId).hide();
 
-      if (input.files && input.files[0]) {
-          var reader = new FileReader();
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
 
-          reader.onload = function (e) {
-              $(imageId).attr('src', e.target.result);
-          };
+        reader.onload = function (e) {
+            $(imageId).attr('src', e.target.result);
+        };
 
-          reader.readAsDataURL(input.files[0]);
+        reader.readAsDataURL(input.files[0]);
 
-          $(imageId).show('1000', 'linear');
-      }
-  }
+        $(imageId).show('1000', 'linear');
+    }
+}
 
 
-  $("#event-image").change(function(){
-      readURL(this, '#event-image-preview');
+$("#event-image").change(function(){
+    readURL(this, '#event-image-preview');
 
-  });
+});
 
-  $("#event-banner").change(function(){
-      readURL(this, '#event-banner-preview');
-  });
+$("#event-banner").change(function(){
+    readURL(this, '#event-banner-preview');
+});
 
-  $("#track-maps").change(function(){
-      readURL(this, '#track-maps-preview');
-  });
+$("#track-maps").change(function(){
+    readURL(this, '#track-maps-preview');
+});
